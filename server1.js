4d801acf3041b46a55b4be5b74148545992c317f
jackycch0373 <164632188+jackycch0373@users.noreply.github.com>
2026-04-09 11:32:09 +0800

    Update server1.js

--- server1.js
+++ server1.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '20mb' }));
 const activeRooms = {};
 // read HTML files
 const getTemplate = (name) => {
-    const filePath = path.resolve(__dirname, 'views', name);
+    const filePath = path.resolve(__dirname, 'Views', name);
     if (!fs.existsSync(filePath)) {
         console.error(`File missing: ${filePath}`);
         return `<h1>Error: File ${name} not found</h1>`;
